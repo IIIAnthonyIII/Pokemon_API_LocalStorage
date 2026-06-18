7b7e0ca90a966d5a773d5b52b374578e950aa6fc
IIIAnthonyIII <aadvl382016@outlook.com>
2026-06-18 11:12:24 -0500

    Update version
    Add Vite + Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img src="./assets/LogoPokemon.png" alt="Logo-Poke" width="320" height="100" class="m-4">
-  <img alt="Vue logo" src="./assets/logo.png" width="120" height="100">
-  <div v-if="state">
-    <ViewPokemon :PokeResultAPI="pokemonesApi.results"/>
-    <PaginationPokemon v-if="showPagination" v-on:goHome="getAllPokemonsAPI" v-on:goToPage="getPokemonsByPagination"/>
+  <div class="block">
+    <div class="m-4">
+      <img src="./assets/LogoPokemon.png" alt="Logo-Poke" width="320" height="100" >
+      <img src="./assets/logo.svg" alt="Vue logo"  width="120" height="100">
+    </div>
+    <div v-if="state">
+      <ViewPokemon :PokeResultAPI="pokemonesApi.results"/>
+      <PaginationPokemon v-if="showPagination" v-on:goHome="getAllPokemonsAPI" v-on:goToPage="getPokemonsByPagination"/>
+    </div>
   </div>
 </template>
 
 <script>
 import ViewPokemon from './components/viewPokemon.vue'
 import PaginationPokemon from './components/paginationPokemon.vue'
+import Env from './environment/env.ts'
 
 export default {
   name: 'App',
@@ -30,7 +35,7 @@ export default {
   },
   methods: {
     async getAllPokemonsAPI(){
-      await this.axios.get(`https://pokeapi.co/api/v2/pokemon`).then(response =>{
+      await this.axios.get(Env.apiUrl).then(response =>{
         this.pokemonesApi = response.data;
         this.pokemonCount = response.data.count;
         this.state = true;
@@ -47,7 +52,7 @@ export default {
       if(offset == 0){
         offset = "0";
       }
-      await this.axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`).then(response =>{
+      await this.axios.get(`${Env.apiUrl}?offset=${offset}&limit=20`).then(response =>{
         this.pokemonesApi = response.data;
       }).catch(error=>{
         console.log(error);
